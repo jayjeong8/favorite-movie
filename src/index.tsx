92c3142b2118f99362d7366a5523384f24f04a7f
jay <zuni4242@gmail.com>
2022-02-05 12:07:48 +0900

    :tada: start project

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {RecoilRoot} from "recoil";
+import{ThemeProvider} from "styled-components";
+import {createGlobalStyle} from "styled-components";
+import {theme} from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+`
+
+
+ReactDOM.render(
+  <React.StrictMode>
+      <RecoilRoot>
+          <ThemeProvider theme={theme}>
+              <GlobalStyle/>
+              <App />
+          </ThemeProvider>
+      </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
