ee7ada7f87b0c3c154a5a31aee284af8ef105d9a
jay <zuni4242@gmail.com>
2022-02-07 10:06:24 +0900

    api - interface IGenres

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,18 @@
 const API_KEY = "8b0c5f0400aa76e404ea70c8b1e0ce22";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IGenres {
+    id: number,
+    name: string;
+}
+
 interface IMovie {
     id: number;
     backdrop_path: string;
     poster_path: string;
     title: string;
     overview: string;
+    genres : IGenres[];
 }
 
 export interface IGetMoviesResult {
