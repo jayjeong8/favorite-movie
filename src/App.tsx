e6a33bc6a4dda7a3cb8c32410861b8462bc03cfe
jay <zuni4242@gmail.com>
2022-02-07 22:01:57 +0900

    search router

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
                     <Route path="/tv" element={<Tv/>}/>
                     <Route path="/tv/*" element={<Tv/>}/>
                     <Route path="/search" element={<Search/>}/>
+                    <Route path="/search?keyword=*" element={<Search/>}/>
                 </Routes>
             </Router>
         </RecoilRoot>
