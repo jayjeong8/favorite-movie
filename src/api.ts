7780f8346de6ac37b7024e6e3599033d8eebb2b1
jay <zuni4242@gmail.com>
2022-02-14 13:59:09 +0900

    movie Api language=ko

--- src/api.ts
+++ src/api.ts
@@ -52,46 +52,41 @@ export interface IBigModal {
 
 
 export function getMovieNowPlaying() {
-    return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`)
-        .then(response => response.json());
-}
-
-export function getMovieLatest() {
-    return fetch(`${BASE_PATH}/movie/latest?api_key=${API_KEY}`)
+    return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=ko`)
         .then(response => response.json());
 }
 
 export function getMovieTopRated() {
-    return fetch(`${BASE_PATH}/movie/top_rated?api_key=${API_KEY}`)
+    return fetch(`${BASE_PATH}/movie/top_rated?api_key=${API_KEY}&language=ko`)
         .then(response => response.json());
 }
 
 export function getMoviePopular() {
-    return fetch(`${BASE_PATH}/movie/popular?api_key=${API_KEY}`)
+    return fetch(`${BASE_PATH}/movie/popular?api_key=${API_KEY}&language=ko`)
         .then(response => response.json());
 }
 
 export function getMovieUpcoming() {
-    return fetch(`${BASE_PATH}/movie/upcoming?api_key=${API_KEY}`)
+    return fetch(`${BASE_PATH}/movie/upcoming?api_key=${API_KEY}&language=ko`)
         .then(response => response.json());
 }
 
 export function getTVAiringToday() {
-    return fetch(`${BASE_PATH}/tv/airing_today?api_key=${API_KEY}`)
+    return fetch(`${BASE_PATH}/tv/airing_today?api_key=${API_KEY}&language=ko`)
         .then(response => response.json());
 }
 
 export function getTVTopRated() {
-    return fetch(`${BASE_PATH}/tv/top_rated?api_key=${API_KEY}`)
+    return fetch(`${BASE_PATH}/tv/top_rated?api_key=${API_KEY}&language=ko`)
         .then(response => response.json());
 }
 
 export function getTVPopular() {
-    return fetch(`${BASE_PATH}/tv/popular?api_key=${API_KEY}`)
+    return fetch(`${BASE_PATH}/tv/popular?api_key=${API_KEY}&language=ko`)
         .then(response => response.json());
 }
 
 export function getTVOnTheAir() {
-    return fetch(`${BASE_PATH}/tv/on_the_air?api_key=${API_KEY}`)
+    return fetch(`${BASE_PATH}/tv/on_the_air?api_key=${API_KEY}&language=ko`)
         .then(response => response.json());
 }
