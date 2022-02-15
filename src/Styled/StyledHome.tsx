bad11cb6ff8cf6acad8011e989e00a4016362d01
jay <zuni4242@gmail.com>
2022-02-15 17:34:57 +0900

    Home style

--- src/Styled/StyledHome.tsx
+++ src/Styled/StyledHome.tsx
@@ -19,15 +19,16 @@ export const Title = styled.h2`
   font-size: 72px;
   font-weight: 300;
   letter-spacing: -4px;
+  margin-left: -1px;
   margin-bottom: 24px;
-  color: ${props => props.theme.white};
+  color: ${props => props.theme.yellow.light};
   filter: drop-shadow(0 0 1.2rem black);
 `;
 export const Overview = styled.p`
   font-size: 16px;
   line-height: 1.6;
   letter-spacing: -1px;
-  margin-left: 8px;
+  margin-left: 4px;
   width: 60%;
   color: ${props => props.theme.gray.light};
 `;
